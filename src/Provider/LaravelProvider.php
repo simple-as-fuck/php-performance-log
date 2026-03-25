db2341c93ccf9ae7b0f260848a7805a6b5a16d9b
VojtaB <triplkrypl@centrum.cz>
2026-03-25 19:47:13 +0100

    usage of global laravel dispatcher for listening DB events to keep old laravel support

--- src/Provider/LaravelProvider.php
+++ src/Provider/LaravelProvider.php
@@ -7,7 +7,6 @@ namespace SimpleAsFuck\PerformanceLog\Provider;
 use Illuminate\Console\Events\CommandFinished;
 use Illuminate\Console\Events\CommandStarting;
 use Illuminate\Contracts\Config\Repository;
-use Illuminate\Database\DatabaseManager;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Database\Events\TransactionBeginning;
 use Illuminate\Database\Events\TransactionCommitted;
@@ -71,18 +70,13 @@ class LaravelProvider extends ServiceProvider
         $dispatcher->listen(CommandStarting::class, static fn (CommandStarting $command) => $consoleListener->onCommandStart($command->command));
         $dispatcher->listen(CommandFinished::class, static fn (CommandFinished $command) => $consoleListener->onCommandFinish($command->command));
 
-        /** @var DatabaseManager $databaseManager */
-        $databaseManager = $this->app->make(DatabaseManager::class);
-        // todo 0.6 use $dispatcher only
-        $databaseDispatcher = $databaseManager->connection()->getEventDispatcher() ?? $dispatcher;
-
         /** @var DatabaseListener $databaseListener */
         $databaseListener = $this->app->make(DatabaseListener::class);
         /** @phpstan-ignore-next-line laravel developers are imbeciles in reality time for now return float|null */
-        $databaseDispatcher->listen(QueryExecuted::class, static fn (QueryExecuted $query) => $databaseListener->onSqlQuery($query->sql, $query->time ?? 0.0, $query->connectionName));
-        $databaseDispatcher->listen(TransactionBeginning::class, static fn (TransactionBeginning $transaction) => $databaseListener->onTransactionStart($transaction->connection->transactionLevel(), $transaction->connectionName));
-        $databaseDispatcher->listen(TransactionRolledBack::class, static fn (TransactionRolledBack $transaction) => $databaseListener->onTransactionFinnish($transaction->connection->transactionLevel(), $transaction->connectionName));
-        $databaseDispatcher->listen(TransactionCommitted::class, static fn (TransactionCommitted $transaction) => $databaseListener->onTransactionFinnish($transaction->connection->transactionLevel(), $transaction->connectionName));
+        $dispatcher->listen(QueryExecuted::class, static fn (QueryExecuted $query) => $databaseListener->onSqlQuery($query->sql, $query->time ?? 0.0, $query->connectionName));
+        $dispatcher->listen(TransactionBeginning::class, static fn (TransactionBeginning $transaction) => $databaseListener->onTransactionStart($transaction->connection->transactionLevel(), $transaction->connectionName));
+        $dispatcher->listen(TransactionRolledBack::class, static fn (TransactionRolledBack $transaction) => $databaseListener->onTransactionFinnish($transaction->connection->transactionLevel(), $transaction->connectionName));
+        $dispatcher->listen(TransactionCommitted::class, static fn (TransactionCommitted $transaction) => $databaseListener->onTransactionFinnish($transaction->connection->transactionLevel(), $transaction->connectionName));
 
         $queueListener = $this->app->make(QueueListener::class);
         /** @phpstan-ignore-next-line laravel developers are imbeciles in reality getJobId for now return int|string */
