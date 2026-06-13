83ddb0d76b5a854bc136aa976f39a562bb289d8d
VojtaB <triplkrypl@centrum.cz>
2026-06-13 14:46:51 +0200

    server request log messages improvement

--- src/Listener/HttpListener.php
+++ src/Listener/HttpListener.php
@@ -38,11 +38,11 @@ class HttpListener
             return;
         }
         if ($threshold === 0.0) {
-            $this->logger->debug('Http request time: '.$time.'ms method: "'.$requestMethod.'" url: "'.$requestUrl.'" pid: '.\getmypid());
+            $this->logger->debug('Http server request time: '.$time.'ms method: "'.$requestMethod.'" url: "'.$requestUrl.'" pid: '.\getmypid());
             return;
         }
         if ($time >= $threshold) {
-            $this->logger->warning('Http request is too slow: '.$time.'ms method: "'.$requestMethod.'" url: "'.$requestUrl.'" threshold: '.$threshold.'ms pid: '.\getmypid());
+            $this->logger->warning('Http server request is too slow: '.$time.'ms method: "'.$requestMethod.'" url: "'.$requestUrl.'" threshold: '.$threshold.'ms pid: '.\getmypid());
         }
     }
 }
