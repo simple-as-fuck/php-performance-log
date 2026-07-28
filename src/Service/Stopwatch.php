3488f96d7db36d0130bee31736ee8a6e4a987dc8
VojtaB <brozek@thepay.eu>
2026-07-28 22:21:56 +0200

    LogicException change to warning log

--- src/Service/Stopwatch.php
+++ src/Service/Stopwatch.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 
 namespace SimpleAsFuck\PerformanceLog\Service;
 
+use Psr\Log\LoggerInterface;
 use SimpleAsFuck\PerformanceLog\Data\Measurement;
 
 class Stopwatch
 {
+    /**
+     * @todo 0.7 make $logger not null
+     */
+    public function __construct(
+        private readonly ?LoggerInterface $logger = null,
+    ){
+    }
+
     public function start(Measurement $measurement, ?string $prefix = null): void
     {
-        $measurement->start($prefix, \microtime(true));
+        $measurement->set($prefix, \microtime(true));
     }
 
     public function finishMilliseconds(Measurement $measurement, ?string $prefix = null): float
@@ -20,6 +29,14 @@ class Stopwatch
 
     public function finishSeconds(Measurement $measurement, ?string $prefix = null): float
     {
-        return \microtime(true) - $measurement->finnish($prefix);
+        $startTime = $measurement->pop($prefix);
+        if ($startTime === null) {
+            $this->logger?->warning('Measurement with prefix: "'.$prefix.'" not started!', [
+                'running_measurements' => $measurement->dump(),
+            ]);
+            return 0;
+        }
+
+        return \microtime(true) - $startTime;
     }
 }
